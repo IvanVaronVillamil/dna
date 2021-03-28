2998e5857968f2b5aca15bef13b67b69aea5ece4
IvanVaronVillamil <ivan.varon@avaldigitallabs.com>
2021-03-27 21:42:11 -0500

    Entrega

---
+++ desarrolloAuth/ADN/src/RsIsMutantDto.java
@@ -0,0 +1,14 @@
+public class RsIsMutantDto {
+
+    private String isMutant;
+
+    public String getIsMutant() {
+        return isMutant;
+    }
+
+    public void setIsMutant(String isMutant) {
+        this.isMutant = isMutant;
+    }
+
+}
+
