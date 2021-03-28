8a09ac7d24bb4292b8498a9a81ae01fa81d08d8e
IvanVaronVillamil <ivan.varon@avaldigitallabs.com>
2021-03-27 21:46:46 -0500

    Lambda AWS isMutant

--- desarrolloAuth/test/src/main/java/co/com/mutant/core/RqIsMutantDto.java
+++ desarrolloAuth/test/src/main/java/co/com/mutant/core/RqIsMutantDto.java
@@ -1,2 +1,17 @@
-package co.com.mutant.core;public class RqIsMutantDto {
+package co.com.mutant.core;
+
+public class RqIsMutantDto {
+
+    private String [] dna;
+
+    public void setDna(String[] args) {
+        this.dna = args;
+    }
+
+    public String[] getDna() {
+        return dna;
+    }
+
+
+
 }
